cb65cb5593b49c87b39f2bf968156edb139fdf02
Majd Haj Hmidi <majdhajhmidi@gmail.com>
2023-08-27 21:29:59 +0300

    Setup main screen Added a main screen that has an app bar and a functional navigation bar

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  static const Map<String, String> _appBarTitleOptions = {
+    'home': 'Home',
+    'folder': 'Select folder',
+    'lecture': 'Lecture details',
+    'calendar': 'Calendar',
+    'report': 'Report',
+  };
+  static const List<String> _tabNames = [
+    'Home',
+    'Lectures',
+    'Calendar',
+    'Report'
+  ];
+  var _navIndex = 0;
+  var _appBarTitle = 'Home';
+
+  void _updateAppBarTitle(String key) {
+    setState(() {
+      _appBarTitle = _appBarTitleOptions[key]!;
+    });
+  }
+
+  void _updateScreen(int index) {
+    setState(() {
+      _navIndex = index;
+      var key = '';
+      switch (_navIndex) {
+        case 0:
+          key = 'home';
+          break;
+        case 1:
+          key = 'folder';
+          break;
+        case 2:
+          key = 'calendar';
+          break;
+        case 3:
+          key = 'report';
+          break;
+      }
+      _updateAppBarTitle(key);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_appBarTitle),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        // This line allows more then three items in the navbar
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _navIndex,
+        onTap: _updateScreen,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.home),
+            label: _tabNames[0],
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.import_contacts),
+            label: _tabNames[1],
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.calendar_month),
+            label: _tabNames[2],
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.equalizer),
+            label: _tabNames[3],
+          ),
+        ],
+      ),
+    );
+  }
+}
